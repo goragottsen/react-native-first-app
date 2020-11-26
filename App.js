9de588e6b4936c2a0c7f3aa27d319940be27c788
goragottsen <goragottsen@gmail.com>
2020-11-25 22:14:35 -0500

    Adding basic inline style to first elements

--- App.js
+++ App.js
@@ -1,23 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   const [outputText, setOutputText] = useState('Open up App.js to start working on your app!')
   return (
-    <View style={styles.container}>
-      <Text>{outputText}</Text>
-      <StatusBar style="auto" />
-      <Button title="Change text" onPress={() => setOutputText('The text changed!')} />
+    <View style={{padding: 50}}>
+      <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}} >
+        <TextInput placeholder="Course Goal" style={{ width: '80%', borderBottomColor: 'black', borderBottomWidth: 1, padding: 10}} />
+        <Button title="ADD" />
+      </View>
+      <View>
+
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
 });
