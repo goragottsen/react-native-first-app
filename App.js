252183f2b3bcd6e8d0d8bea948808d92f47fa5bf
goragottsen <goragottsen@gmail.com>
2020-11-27 22:58:21 -0500

    Added FlatView to output list

--- App.js
+++ App.js
@@ -1,22 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList } from 'react-native';
 
 export default function App() {
-  const [outputText, setOutputText] = useState('Open up App.js to start working on your app!')
+  const [enteredGoal, setEnteredGoal] = useState('')
+  const [courseGoals, setCourseGoals] = useState([])
+
+  const goalInputHandler = enteredText => {
+    setEnteredGoal(enteredText)
+  }
+
+  const enteredGoalHandler = () => {
+    setCourseGoals(currentGoals => [...currentGoals, {id: Math.random().toString(), val: enteredGoal}])
+  }
+
   return (
     <View style={styles.screen}>
       <View style={styles.inputContainer} >
         <TextInput 
           placeholder="Course Goal" 
-          style={styles.input} 
+          style={styles.input}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
         />
-        <Button title="ADD" />
+        <Button title="ADD" onPress={enteredGoalHandler} />
       </View>
-      <View>
-
-      </View>
-    </View>
+      <FlatList
+        keyExtractor={(item, index) => item.id}
+        data={courseGoals} 
+        renderItem={
+          itemData => (
+              <View style={styles.listItem}>
+                <Text >{itemData.item.val}</Text>
+              </View>
+            )
+          }>
+      </FlatList>
+    </View> 
   );
 }
 
@@ -34,5 +54,12 @@ const styles = StyleSheet.create({
     borderBottomColor: 'black', 
     borderBottomWidth: 1, 
     padding: 10
+  },
+  listItem: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: '#ccc',
+    borderColor: 'black',
+    borderWidth: 1
   }
 });
